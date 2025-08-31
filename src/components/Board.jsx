1dfbcb8ac113d93d02bece781095d2713bf5c964
NguyenPhucDinhQuyen <npdquyen1142@gmail.com>
2025-08-31 17:30:26 +0700

    add logic game

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,11 +1,69 @@
-import Card from './Card.jsx'
+import { useState, useEffect } from 'react';
+import Card from './Card.jsx';
+
+const logicGame = (cards) => {
+    // Check win
+    for(let i = 0; i < 3; i++){
+        if(cards[3*i] && cards[3*i+1] && cards[3*i + 2]){
+            if(cards[3*i] == cards[3*i+1] && cards[3*i+1] == cards[3*i + 2]){
+                return 1;
+            }
+        }
+
+        if(cards[i] && cards[i+3] && cards[i + 6]){
+            if(cards[i] == cards[i+3] && cards[i+3] == cards[i + 6]){
+                return 1;
+            }
+        }
+
+    }
+    if(cards[0] && cards[4] && cards[8] && 
+    cards[0] == cards[4] && cards[4] == cards[8]){
+        return 1;
+    }
+
+    
+    if(cards[2] && cards[4] && cards[6] && 
+    cards[2] == cards[4] && cards[4] == cards[6]){
+        return 1;
+    }
+
+    // Check tie
+    if (cards.every(Boolean)) return 0; 
+
+    return -1; // Not ending
+}
 
 function Board() {
-        const cards = Array(9).fill(null);
+        const [cards, setCards] = useState(Array(9).fill(null))
+        const [value, setValue] = useState(0)
+        const handleCardClick = (index) => {
+            if(cards[index]){
+                return;
+            }
+
+            const newCards = [...cards]
+            
+            newCards[index] = value ? 'X' : 'O'
+            if(logicGame(cards) == 1){
+                console.log(value, 'win')
+                return;
+            } else if(logicGame(cards) == 0){
+                console.log('draw')
+                return;
+            }
+            setCards(newCards)
+
+        };
+        useEffect(()=>{
+            setValue(value^1);
+        }, cards)
         return (
         <div className='grid grid-cols-3 gap-4 '>
-            {cards.map((_, index) => (
-                <Card key={index} />
+            {cards.map((value,index ) => (
+                <Card key={index}
+                      value={value}
+                      onClick = {()=>handleCardClick(index)} />
             )) }
         </div>
     )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,10 @@
-function Card() {
+function Card({value, onClick}) {
     return (
             <button className="text-2xl bg-red-200
              h-[100px] w-[100px] text-center
              border-2 border-black
-             hover:bg-gray-200">
-                X
+             hover:bg-gray-200" onClick={onClick}>
+                {value}
             </button>
         
 
